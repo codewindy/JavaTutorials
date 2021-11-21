3dcbdeb221d28863303e6945b745ee493cf92cf5
codewindy <bravohex@gmail.com>
2021-11-21 15:20:25 +0800

    update spring batch README.md

---
+++ mongodb/src/main/java/com/codewindy/mongodb/service/Caculator.java
@@ -0,0 +1,9 @@
+package com.codewindy.mongodb.service;
+
+public interface Caculator {
+
+    int add (int a, int b);
+    int minus (int a, int b);
+    int multiply (int a, int b);
+    int divide (int a, int b);
+}
